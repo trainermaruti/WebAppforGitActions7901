a963a51af2dd4d59a91e18ffaa5bd53d006c4011
maruti <maruti@MarutiDevVM7901>
2025-04-25 08:52:41 +0000

    Add project files.

---
+++ WebAppforGitActions7901/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppforGitActions7901.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
